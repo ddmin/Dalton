926b994af5fb0fc5a6d43f058b76828d8e1b26ef
Daniel Min <32945331+ddmin@users.noreply.github.com>
2018-10-20 00:41:23 -0400

    Update dalton.py

--- dalton.py
+++ dalton.py
@@ -7,6 +7,8 @@ import pyautogui
 import time
 from desmos_interface import screenshot
 from desmos_interface import setup
+from desmos_interface import delete
+from desmos_interface import show_graph
 
 setup()
 
@@ -17,6 +19,7 @@ client = commands.Bot(command_prefix = '!')
 @client.event
 async def on_ready():
     print('Dalton is online')
+    print()
 
 @client.event
 async def on_message(message):
@@ -24,17 +27,65 @@ async def on_message(message):
 
     if message.content.startswith('#help'):
         await client.send_message(channel, 'I am Dalton, the Graphing Calculator' \
-                                  ' assistant. You can summon me by typing "#graph".' \
-                                  '\nEx: #graph (x^3-x)/(2x)')
+                                  ' assistant. You can summon me by typing "#graph" followed by a number and an equation.' \
+                                  '\nEx: #graph1 (x^3-x)/(2x)' \
+                                  '\n\nFunctions:\n' \
+                                  '#graph - Displays current graph\n' \
+                                  '#graph[n] (equation) - Graphs equation in graph n (where n is an integer 1-3)\n' \
+                                  '#delete [n] - Deletes graph n (where n is an integer 1-3)\n' \
+                                  '#delete all - Deletes all graphs')
 
-    if message.content.startswith('#graph ') and not message.author.bot:
-        equation = message.content[7:]
-        await client.send_message(channel, f'Graph of {equation}')
-        pyperclip.copy(equation)
-        await client.send_message(channel, f'Loading graph...')
-        screenshot()
+    if message.content.startswith('#graph') and message.content != '#graph' and not message.author.bot:
+
+        try:
+            number = int(message.content[6])
+            equation = message.content[8:]
+
+            print("Graph:", number)
+            print("Equation:", equation)
+            print()
+
+            if screenshot(number, equation) is True:
+                await client.send_message(channel, f'Graph of {equation}')
+
+                await client.send_message(channel, f'Loading graph...')
+                await client.send_file(channel, 'graph.png')
+
+            else:
+                await client.send_message(channel, "I can only graph up to 3 graphs")
+
+        except:
+            await client.send_message(channel, "I'm afraid I can't do that.")  
+
+
+    if message.content.startswith('#delete all') and not message.author.bot:
+        for _ in range(10):
+            boolean = delete(1)
+            
+        await client.send_message(channel, f"Deleted all graphs.")
+
+    if message.content.startswith('#delete ') and not message.content.startswith('#delete all') and not message.author.bot:
+        try:
+            number = int(message.content[8])
+
+            print("Deleting graph", number)
+            print()
+
+            if delete(number) is True:
+                await client.send_message(channel, f"Deleted graph {number}.")
+            else:
+                await client.send_message(channel, f"I can only graph up to 3 graphs.")
+            
+        except:
+            await client.send_message(channel, "I'm afraid I can't do that.")
+
+    if message.content == '#graph' and not message.author.bot:
+        show_graph()
+        await client.send_message(channel, "Displaying current graph...")
         await client.send_file(channel, 'graph.png')
         
 client.run(TOKEN)
 
-#TODO: Add constants and type each character
+#TODO:
+    #Zoom in and out
+    #Move graph
